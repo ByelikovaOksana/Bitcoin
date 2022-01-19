fe483e9ed314691edd5d076ed66abdbb648384ae
ByelikovaOksana <byelikova.oksana@gmail.com>
2022-01-19 17:06:06 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+window.$ = window.jQuery = require('jquery');
+
+
+//Mobile menu
+$('.menu-icon').on('click', function() {
+    $('.menu').addClass('active');
+})
+$('.cancel-icon').on('click', function() {
+    $('.menu').removeClass('active');
+});
